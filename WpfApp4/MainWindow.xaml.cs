5a3d0aa5042effcbf26e484244a1fa7601471c19
aleksankaa <keksikzh@gmail.com>
2024-06-18 15:17:44 +0400

    lala

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -50,14 +50,23 @@ namespace fkfk
             return MathF.Round(weight / MathF.Pow(height / 100, 2), 2);
         }
 
-        private string GetBMIStatus(float bmi)
+         private string GetBmiStatus(float bmi)
         {
-            var bmiStatusRanges = new (float, float, string)[]
+            (float MinValue, float, string, Brush)[] bmiStatusRanges = new (float MinValue, float, string, Brush)[]
             {
-                  (float.MinValue, 18.5f, "Недостаточный вес"),
-                  (18.5f, 24.9f, "Нормальный вес"),
-                  (25.0f, 29.9f, "Избыточный вес"),
-                  (30.0f, float.MaxValue, "Ожирение"),
+                (float.MinValue, 18.5f, "Недостаточный вес \n Имеется риск угрозы здоровья",
+                    Foreground = Brushes.LightGreen),
+                (18.5f, 24.9f, "Нормальный вес", Foreground = Brushes.Chartreuse),
+                (25.0f, 27.9f, "Избыточный вес \n Желательно уменьшение массы тела", Foreground = Brushes.Orange),
+                (28.0f, 30.9f, "Ожирение первой степени \n Желательно уменьшение массы тела",
+                    Foreground = Brushes.IndianRed),
+                (31.0f, 35.9f, "Ожирение второй степени \n Крайне желательно уменьшение массы тела",
+                    Foreground = Brushes.OrangeRed),
+                (36.0f, 40.9f, "Ожирение третьей степени \n Крайне желательно уменьшение массы тела",
+                    Foreground = Brushes.Red),
+                (41.0f, float.MaxValue,
+                    "Ожирение четвертой степени \n Необходимы срочные меры по уменьшению массы тела",
+                    Foreground = Brushes.DarkRed),
             };
             foreach (var (min, max, status) in bmiStatusRanges)
             {
